9ea07124b02bcc4b374b6217ec2f066d9e0bfb09
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2018-05-13 21:43:13 +0100

    things!

--- platforms/browser/www/js/canvas.js
+++ platforms/browser/www/js/canvas.js
@@ -1,14 +1,17 @@
 var canvas = document.getElementById("cvs");
 var ctx = canvas.getContext("2d");
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
 canvas.addEventListener('click', function(event) {
-    ctx.fillStyle="#0000FF";
-    ctx.fillRect(event.pageX, event.pageY, event.pageX+50, event.pageX+50);
+    drawCard(ctx, event.x, event.y, 20);
 }, false);
 ctx.fillStyle="#FF0000";
 ctx.fillRect(20,20,150,150);
 
 function drawCard(ctx, posX, posY, number) {
-    ctx.rect(x, y, 60, 100);
+    ctx.fillStyle="white";
+    ctx.fillRect(posX, posY, 60, 100);
+    ctx.rect(posX, posY, 60, 100);
+    ctx.strokeStyle="black";
+    ctx.stroke();
 }
-
-drawCard(ctx, 150, 150, 20);
